336eabdc26eed68dcc96495248b9e27d0be46f72
Harsh Kumar <connect.to.hk@gmail.com>
2024-08-15 22:03:14 +0530

    fix: redirect fix

--- src/app/api/problem/getall/route.ts
+++ src/app/api/problem/getall/route.ts
@@ -4,9 +4,8 @@ import clientPromise from "@/lib/mongodb";
 export async function GET(request: Request) {
     try {
         const client = await clientPromise;
-        const db = client.db();
-        const problemsCollection = db.collection("problems");
-        const result = await problemsCollection.find({}).sort({ created_at: -1 }).toArray();
+        const result = await client.db().collection("problems").find({}).sort({ created_at: -1 }).toArray();
+        console.log(result)
         return NextResponse.json(result, { status: 200 });
     } catch (err) {
         return NextResponse.json(err, { status: 500 });
