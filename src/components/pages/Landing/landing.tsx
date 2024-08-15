b9b386ddae503d928ede67caed50f82b6b5f5f77
Harsh Kumar <connect.to.hk@gmail.com>
2024-08-15 20:27:37 +0530

    feature: add the problem

--- src/components/pages/Landing/landing.tsx
+++ src/components/pages/Landing/landing.tsx
@@ -9,19 +9,21 @@ export default function Landing() {
     const { isLoading, problems, error } = useProblem()
     return (
         <div className="container py-6 px-4 md:px-6">
-
-            {isLoading == false && problems == null ? (
-                <div className="flex w-full justify-center my-10">
-                    <div className="font-semibold text-2xl">NO PROBLEMS AVAILABLE TO SHOW RIGHT NOW</div>
-                </div>
-            ) : (<div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
-                {problems && problems.map((prob: Problem, i: any) => (<Problemcard key={i} prob={prob} />)
-                )}
-            </div>
-            )
-            }
-
+            {!isLoading && (
+                problems && problems.length === 0 ? (
+                    <div className="flex w-full justify-center my-10 ">
+                        <div className="font-semibold text-2xl">NO PROBLEMS AVAILABLE TO SHOW RIGHT NOW, BE THE FIRST ONE TO POST!!</div>
+                    </div>
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        {problems.map((prob: Problem, i: number) => (
+                            <Problemcard key={i} prob={prob} />
+                        ))}
+                    </div>
+                )
+            )}
         </div>
+
 
     )
 }
